13fe14fac0d12191122beda7eb686fb30c0e5dd2
U_JINYEOL <100745412+U-Jinyeol@users.noreply.github.com>
2024-01-12 20:35:05 +0900

    [TEST] branch rull test

--- src/hooks/querys/test.ts
+++ src/hooks/querys/test.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useFetchTest = () => {
   return useQuery({
-    queryKey: ["getTest,test"],
+    queryKey: ["getTest"],
     queryFn: _getTest,
   });
 };
