366b4aa2b7a733ebc1eeac50a7c454419b03a41f
hyunjin <guswls1419@tpirates.com>
2024-01-19 23:07:59 +0900

    :sparkles: test

--- src/components/Layouts/SideBar.tsx
+++ src/components/Layouts/SideBar.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const SideBar = () => {
   return (
     <React.Fragment>
       <aside>
         <div className="fixed z-49 top-14 inset-0 w-60 min-h-screen p-2 bg-gray-500">
-          <h1 className="text-40px">SIDE BAR</h1>
+          <h1 className="text-40px">SIDE BARdfd</h1>
         </div>
       </aside>
     </React.Fragment>
