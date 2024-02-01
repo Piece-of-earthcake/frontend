f69be95dc7047041579c0787deab96f7daee81e1
hyunjin <guswls1419@tpirates.com>
2024-02-02 01:09:12 +0900

    feat : fragment 삭제

--- src/components/Home/Notification/Scales.tsx
+++ src/components/Home/Notification/Scales.tsx
@@ -36,22 +36,20 @@ const Scales = ({ data }: { data: number }) => {
   }, [data]);
 
   return (
-    <>
-      <div className="animation-bottom-up relative mr-2 h-20 min-w-48">
-        <Image
-          alt="scales"
-          className={clsx(
-            'absolute bottom-1 left-22 z-10 h-16 w-4 origin-bottom transform object-cover object-center transition-transform duration-500',
-            rotationClass
-          )}
-          priority
-          width={60}
-          height={80}
-          src="/scales_needie.png"
-        />
-        <Image alt="scales" priority src="/scales.png" sizes="100%" fill />
-      </div>
-    </>
+    <div className='animation-bottom-up relative mr-2 h-20 min-w-48'>
+      <Image
+        alt='scales'
+        className={clsx(
+          'absolute bottom-1 left-22 z-10 h-16 w-4 origin-bottom transform object-cover object-center transition-transform duration-500',
+          rotationClass
+        )}
+        priority
+        width={60}
+        height={80}
+        src='/scales_needie.png'
+      />
+      <Image alt='scales' priority src='/scales.png' sizes='100%' fill />
+    </div>
   );
 };
 
